69123dd47e876866b373257d8af892c236cd72e0
wikigods <wikigods@gmail.com>
2024-12-18 23:10:38 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://wikigods.github.io/my-react-app",
+  base: "my-react-app",
   plugins: [react()],
-  root: './',  // Asegúrate de que sea la raíz de tu proyecto
-  build: {
-    outDir: 'dist',  // La carpeta donde se generará el build
-  }
 })
